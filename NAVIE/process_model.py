e9c347bc2e1503c29aa8f3f0ad348183355b0e06
Arya Marda <111842640+Arya-Ayra@users.noreply.github.com>
2023-11-06 11:16:01 +0530

    Update process_model.py

--- NAVIE/process_model.py
+++ NAVIE/process_model.py
@@ -1,8 +1,11 @@
 from ultralytics import YOLO
+if __name__ == '__main__':
 
-for m in {'yolov5nu', 'yolov5su', 'yolov5mu', 'yolov5lu', 'yolov5xu'}:
-    #models[m] = torch.hub.load('ultralytics/ultralytics', m, force_reload=False, device='cpu')
-    z = m + ".pt"
+    models = {}
+    for m in {'yolov5n', 'yolov5s', 'yolov5m', 'yolov5l', 'yolov5x'}:
+        #models[m] = torch.hub.load('ultralytics/ultralytics', m, force_reload=False, device='cpu')
+        z = m + ".pt"
+        models[m] = YOLO(z)
+    # logger.info(    {'Component': "Process" , "Action": "Model's loaded ready to start processing" }  ) 
 
-print("Model loaded")
- 
+    print("Model Loaded")
